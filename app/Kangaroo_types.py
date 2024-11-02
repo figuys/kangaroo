796df7627f31cef725fe9cfc8c030b84cc7cedce
Paul Murphy <paul.d.murphy@gmail.com>
2024-11-02 12:01:00 -0400

    converts the puzzle public keys byte madness.

---
+++ app/Kangaroo_types.py
@@ -0,0 +1,3 @@
+type puzzle_pubkey_item = tuple[
+    int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int]
+type puzzle_pubkey_list = list[puzzle_pubkey_item]
